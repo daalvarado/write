ae099c4989c421ce5d1ce785a0eba154d028199a
Diego Alvarado <diego23k@gmail.com>
2018-03-11 18:15:54 +0100

    con calculo tiempos funcionando

--- src/js/timeService.js
+++ src/js/timeService.js
@@ -1,11 +1,33 @@
 export class TimeService{
 
-timeStamp() {
-    $('.timestamp').each(function (index,value){
-let timePassed=moment($(this)[0].innerText).fromNow();
-$(this).siblings(".timeCalc").html("Published " + timePassed)
-    })
 
+
+
+timeStamp() {
+     
+
+    $('.timestamp').each(function (index,value){
+        let now= moment();
+        let pubDate = moment($(this)[0].innerText);
+        let daysAgo = now.diff(pubDate, "days");
+        let hourAgo = now.diff(pubDate, "hours")
+        if (hourAgo>168) {
+            let a = moment(pubDate).format("MMMM D, YYYY");
+            $(this)
+              .siblings(".timeCalc")
+              .html("Published on " + a);
+        } else if (hourAgo>24) {
+            let a = moment(pubDate).format("dddd");
+            $(this)
+              .siblings(".timeCalc")
+              .html("Published on " + a);
+        } else {
+let timePassed = moment($(this)[0].innerText).fromNow();
+$(this)
+  .siblings(".timeCalc")
+  .html("Published " + timePassed);
+        }
+    })
 }}
 
 // window.onload=function(){
